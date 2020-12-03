dfc1245370be238534715561c1955018a45114e7
sultanmrf <sultanfanody@gmail.com>
2020-12-03 17:02:01 +0330

    ADD to function updatepost,storepost

--- Classes/Post.php
+++ Classes/Post.php
@@ -18,4 +18,34 @@ class Post extends Model
     {
         // implement calculateCountViews function ...
     }
+    function updatePost($id, $title, $short_description, $description,$select_cate)
+    {
+        $query = "UPDATE `posts` SET 
+        `categories_id` = '{$select_cate}',
+        `title` = '{$title}', 
+        `short_description` = '{$short_description}',
+        `description` = '{$description}'
+         WHERE `id` = '{$id}'";
+        return $this->conn->query($query);
+
+    }
+    function storePost($title,$short_description,$description,$file,$select_cate)
+    {
+        $extension = explode('.', $file);
+        $extension = end($extension);
+        $file_name = time() . '.' . $extension;
+        $upload = "posts/image-post/'.'$file_name";
+        !is_dir("posts/image-post") ? mkdir("image-post") : '';
+        if(move_uploaded_file($_FILES['file']['tmp_name'], $upload) == true) {
+            $query = "INSERT INTO `posts`(`id`,`categories_id`, `title`, `short_description`, `description`, `count_views`, `pic_url`, `created_at`) VALUES
+      (NULL,'{$select_cate}', '{$title}', '{$short_description}', '{$description}', '0', '$file_name', '" . date('Y-m-d H:i:s') . "')";
+
+            return $this->conn->query($query);
+        }
+        else
+        {
+            echo "<script>alert('آپبود فایل درست انجام نشد')</script>";
+        }
+    }
+
 }
