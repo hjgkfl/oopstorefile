e9cb0448aabea2355154a7d8a3ec2049d6d7c510
sultanmrf <sultanfanody@gmail.com>
2020-12-03 15:46:40 +0330

    add function store and pagenavi and commentcount

--- Classes/Comment.php
+++ Classes/Comment.php
@@ -11,7 +11,7 @@ class Comment extends Model
 
     public function getPostComment($post_id, $parent_id = NULL)
     {
-        $query = "SELECT * FROM `comments` WHERE `post_id` = '{$post_id}'";
+        $query = "SELECT * FROM `comments` WHERE `post_id` = '{$post_id}' AND `is_confirm` = 1 ";
         if(is_null($parent_id)) {
             $query .= " AND `parent_id` = '0'";
         } else {
@@ -26,6 +26,18 @@ class Comment extends Model
      (NULL,  '{$params['post_id']}','{$params['parent_id']}','{$params['name']}','{$params['mobile']}','{$params['email']}','{$params['description']}', '0', '" . date('Y-m-d H:i:s') . "')";
 
         $this->conn->query($query);
+    }
+
+    public function pageNavi($post_id,$parent_id,$limit,$offset)
+    {
+        $query = "SELECT * FROM `comments` WHERE `post_id`=$post_id AND `is_confirm`= 1  AND `parent_id`=$parent_id  LIMIT  $limit OFFSET $offset ";
+        return $this->conn->query($query);
+    }
+
+    function commentCount($page)
+    {
+        return ($page - 1) * 5;
+
     }
 
 }
