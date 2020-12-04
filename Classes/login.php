ec5e27d5a717e2f15183cf5a07e63f8edd7916ca
sultanmrf <sultanfanody@gmail.com>
2020-12-04 16:08:12 +0330

    add to function checkDate to file class login.php

--- Classes/login.php
+++ Classes/login.php
@@ -3,20 +3,22 @@
 
 namespace Classes;
 
+session_start();
 
 class login extends Model
 {
     function loginCheck($email, $password)
     {
-        $query = "SELECT * FROM `users` WHERE `email`='$email' AND `password`='$password' ";
+        $query = "SELECT * FROM `users` WHERE `email`='{$email}' AND `password`='{$password}' ";
         $result = $this->conn->query($query);
         $run = $result->fetch();
-        if ($num_rows = $result->rowCount() == 1) {
+        $num_rows = $result->rowCount();
+        if($num_rows == 1) {
             $t = $run['id'];
             $sql = "SELECT `user_status` FROM users WHERE `id`=$t ";
             $run1 = $this->conn->query($sql);
             $row = $run1->fetch();
-            if ($row['user_status'] == 1) {
+            if ($row['user_status'] == self::Role_admin) {
                 return [$_SESSION['login'] = 1, $_SESSION['user_status'] = $run['user_status'], $_SESSION['name'] = $run['l-f-name'], $_SESSION['email'] = $run['email'], header("location:admin/dashboard.php")];
 
             } else {
@@ -28,11 +30,21 @@ class login extends Model
         }
 
     }
-    function echoError($error,$c)
+    function checkDate($email,$password)
     {
-        for($i=0;$i<$c;$i++)
+        $error=[];
+        empty($email) ? array_push($error,"ایمیل خود را وارد  نکردید") : '';
+        if(empty($password))
         {
-            echo "<script>alert('$error[$i]')</script>";
+            array_push($error,"رمز عبور خود را وارد  نکردید");
         }
+        else
+        {
+            if((preg_match("/^(?=.*[A-z])(?=.*[0-9])$/",$password)))
+                array_push($error,"رمز عبور  که وارد کردید صحیح نیست !  رمز عبور شما باید دارای حروف کوچک و بزرگ انگلیسی باشد و باید دارای اعداد هم باشد");
+        }
+        return $error;
+
     }
+
 }
